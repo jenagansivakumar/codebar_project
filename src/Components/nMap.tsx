eb0acbabf17eeef03c758a5ebad8575b2f8ab211
Jena <jenagansivakumar@gmail.com>
2023-09-22 15:48:37 +0200

    added tailwind css

--- src/Components/nMap.tsx
+++ src/Components/nMap.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function hello() {
-  return <div>hello</div>;
+interface nMapComponent {
+  scan(host: string): Promise<string>;
+}
+
+export default function Nmap(nMapComponent) {
+  return (
+    <form>
+      <div>
+        <label htmlFor="Host">Host</label>
+        <input type="text" name="Host" placeholder="Host" />
+      </div>
+      <div>
+        <label>Scan</label>
+        <input type="text" name="Scan" placeholder="Scan" />
+      </div>
+      <div></div>
+    </form>
+  );
 }
